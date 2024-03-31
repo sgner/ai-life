e2a28fed931849ab9ce2c9ff76e612f1c83ad6c7
sgner <cfwrettt@gmail.com>
2024-04-01 00:48:42 +0800

    Initial commit

---
+++ ai-life-service/src/main/java/com/life/ai/AiLifeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.life.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLifeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiLifeServiceApplication.class, args);
+    }
+
+}
